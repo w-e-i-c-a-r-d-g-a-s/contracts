2eb5a33ee1b277d39074d9c2cfce110c77027158
kyohei8 <tsukuda.kyouhei@gmail.com>
2017-08-26 21:36:06 +0900

    update gasPrice

--- truffle.js
+++ truffle.js
@@ -3,7 +3,8 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gasPrice: 0x05e4accdd9 // 25311366617 Gwei
     },
     coverage: {
       host: "localhost",
